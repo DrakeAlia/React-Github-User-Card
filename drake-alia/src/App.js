6c80cc9806c5c577cb62f8a7534487b1d76a4464
Drake Alia <DrakeTesla@MacBook-Pro.local>
2020-05-05 17:18:25 -0700

    added app.js

--- drake-alia/src/App.js
+++ drake-alia/src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      User: {}, 
+      Follwers: [],
+      Searchfollowers: ""
+    };
+  }
+  componentDidMount() {
+    axios
+    .get("https://api.github.com/users/DrakeAlia") 
+    .then(res => {
+      console.log("userData", res.data);
+      this.setState({
+        ...this.state,
+        User: res.data
+      });
+    })
+    .catch(err => console.log("errorUsers", err));
+
+ axios
+ .get("https://api.github.com/users/DrakeAlia/Followers")
+ .then(response => {
+   console.log(response);
+   this.setState({
+     Followers: response.data
+   });
+ })
+ .catch(err => console.log("errorFollowers", err));
+}
+
+// handleSearchFollowers = e => {
+//   this.setState 
+// }
+
+render() {
+  console.log(this.state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>My Github Usercard</h1>
+    {/* <input 
+     type="text"
+     value={this.state.doggoText}
+     onChange={this.handleChanges}
+   /> */}
     </div>
-  );
+
+    )
+  }
 }
 
 export default App;
